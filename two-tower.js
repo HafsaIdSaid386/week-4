6e74931bf4533e7d663ce42326cdeb7c7bc20957
HafsaIdSaid386 <hafosatihihi@gmail.com>
2025-10-02 22:32:00 +0100

    two-tower.js

--- two-tower.js
+++ two-tower.js
@@ -3,72 +3,40 @@ class TwoTowerModel {
     this.numUsers = numUsers;
     this.numItems = numItems;
     this.embeddingDim = embeddingDim;
-
     this.userEmbeddings = tf.variable(tf.randomNormal([numUsers, embeddingDim], 0, 0.05));
     this.itemEmbeddings = tf.variable(tf.randomNormal([numItems, embeddingDim], 0, 0.05));
-
     this.optimizer = tf.train.adam(lr);
   }
 
-  userForward(userIndices) {
-    return tf.gather(this.userEmbeddings, userIndices);
-  }
-
-  itemForward(itemIndices) {
-    return tf.gather(this.itemEmbeddings, itemIndices);
-  }
-
-  score(userEmbeddings, itemEmbeddings) {
-    // dot product along last dim
-    return tf.sum(tf.mul(userEmbeddings, itemEmbeddings), -1);
-  }
+  userForward(userIndices) { return tf.gather(this.userEmbeddings, userIndices); }
+  itemForward(itemIndices) { return tf.gather(this.itemEmbeddings, itemIndices); }
+  score(userEmbeddings, itemEmbeddings) { return tf.sum(tf.mul(userEmbeddings, itemEmbeddings), -1); }
 
   async trainStep(userIndices, itemIndices) {
     const userTensor = tf.tensor1d(userIndices, 'int32');
     const itemTensor = tf.tensor1d(itemIndices, 'int32');
-
     let lossTensor;
     this.optimizer.minimize(() => {
       const userEmbs = this.userForward(userTensor);
       const itemEmbs = this.itemForward(itemTensor);
-      const scores = this.score(userEmbs, itemEmbs); // shape [batch]
+      const scores = this.score(userEmbs, itemEmbs);
       const labels = tf.onesLike(scores);
-      const loss = tf.losses.meanSquaredError(labels, scores); // shape [batch]
-      const meanLoss = tf.mean(loss); // scalar
+      const loss = tf.losses.meanSquaredError(labels, scores);
+      const meanLoss = tf.mean(loss);
       lossTensor = meanLoss;
       return meanLoss;
     });
-
     const lossVal = lossTensor.dataSync()[0];
-
-    // cleanup
-    userTensor.dispose();
-    itemTensor.dispose();
-    lossTensor.dispose();
-
+    userTensor.dispose(); itemTensor.dispose(); lossTensor.dispose();
     return lossVal;
   }
 
-  // Helpers used by app.js
-  getUserEmbedding(userIndex) {
-    return tf.tidy(() => {
-      const idx = tf.tensor1d([userIndex], 'int32');
-      return this.userForward(idx).squeeze(); // shape [D]
-    });
-  }
-
-  getItemEmbeddings() {
-    // caller may read arraySync() for viz
-    return this.itemEmbeddings;
-  }
-
+  getUserEmbedding(userIndex) { return tf.tidy(() => this.userForward(tf.tensor1d([userIndex], 'int32')).squeeze()); }
+  getItemEmbeddings() { return this.itemEmbeddings; }
   async getScoresForAllItems(userEmb) {
     return tf.tidy(() => {
-      const scores = tf.matMul(
-        userEmb.reshape([1, this.embeddingDim]),
-        this.itemEmbeddings.transpose()
-      ); // [1, numItems]
-      return scores.dataSync(); // Float32Array
+      const scores = tf.matMul(userEmb.reshape([1, this.embeddingDim]), this.itemEmbeddings.transpose());
+      return scores.dataSync();
     });
   }
 }
