8e6a3310abe2d61bca0958f8dfa24e9e5aabc15d
HafsaIdSaid386 <hafosatihihi@gmail.com>
2025-10-06 18:17:56 +0100

    Update two-tower.js

--- two-tower.js
+++ two-tower.js
@@ -1,18 +1,16 @@
-// two-tower.js - PROVEN WORKING VERSION
+// two-tower.js - FIXED WORKING VERSION
 class TwoTowerModel {
   constructor(numUsers, numItems, numGenres, embDim = 32, hiddenDim = 64, lr = 0.01) {
     this.numUsers = numUsers;
     this.numItems = numItems;
+    this.numGenres = numGenres;
     this.embDim = embDim;
     
-    // Simple embeddings - NO MLP for stability
+    // Simple embeddings
     this.userEmbedding = tf.variable(tf.randomUniform([numUsers, embDim], -0.05, 0.05));
     this.itemEmbedding = tf.variable(tf.randomUniform([numItems, embDim], -0.05, 0.05));
-    
-    // Genre weights
     this.genreWeights = tf.variable(tf.randomUniform([numGenres, embDim], -0.05, 0.05));
     
-    // Higher learning rate
     this.optimizer = tf.train.adam(lr);
   }
 
@@ -34,22 +32,22 @@ class TwoTowerModel {
     return this.itemForward(itemIdxTensor, genreTensor);
   }
 
-  async trainStep(userIdx, itemIdx, genreBatch) {
+  async trainStep(userIdx, itemIdx, genreBatch, globalItemGenreTensor) {
     const lossVal = await this.optimizer.minimize(() => {
       return tf.tidy(() => {
         const u = this.userForward(userIdx);
         const i_pos = this.itemForward(itemIdx, genreBatch);
         
-        // Simple dot product scores
+        // Positive scores
         const pos_scores = tf.sum(tf.mul(u, i_pos), 1);
         
-        // Sample random negatives
+        // Sample random negatives - FIXED: use the passed globalItemGenreTensor
         const neg_indices = tf.randomUniform(itemIdx.shape, 0, this.numItems, 'int32');
         const neg_genres = tf.gather(globalItemGenreTensor, neg_indices);
         const i_neg = this.itemForward(neg_indices, neg_genres);
         const neg_scores = tf.sum(tf.mul(u, i_neg), 1);
         
-        // BPR Loss: -log(sigmoid(pos_score - neg_score))
+        // BPR Loss
         const diff = tf.sub(pos_scores, neg_scores);
         const loss = tf.neg(tf.mean(tf.logSigmoid(diff)));
         
